ebb7a23afc6ab68cdf69e45819902ec76d18839f
Pharos1011 <90761541+Phanos1011@users.noreply.github.com>
2021-11-25 03:06:49 +0200

    Update Learn.go

--- Learn.go
+++ Learn.go
@@ -1,35 +1,4 @@
-/*package main
 
-import (
-	"fmt"
-	"io"
-	"log"
-	"net/http"
-	"os"
-)
-
-func main() {
-	//var mydata string[];
-	// запрос в формате http
-	//response, request := http.Get("http://localhost:8000/_/api/")
-	response, request := http.Get("http://localhost:8000/_/api/v3/key_values")
-
-	if request != nil {
-		log.Fatal("failed to read file:",request) //Fatal is equivalent to Print() followed by a call to os.Exit(1)
-	}
-	defer response.Body.Close()  //закрываем файл defer до выхода из функции мейн
-
-
-
-	// копируем инфо в нормальный вывод
-	n, request := io.Copy(os.Stdout, response.Body)
-	if request != nil {
-		log.Fatal("failed to copy file",request)
-	}
-
-	fmt.Println("number of bytes:", n)
-}
-*/
 
 package main
 import (
